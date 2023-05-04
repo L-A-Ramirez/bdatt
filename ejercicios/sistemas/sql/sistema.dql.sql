8e5162bdd726c8ffe4655ba1d7d6807d70385411
L-A-Ramirez <Lucas4Alejandro4@gmail.com>
2023-05-04 15:24:51 -0300

    update bda tnot v3

--- ejercicios/sistemas/sql/sistema.dql.sql
+++ ejercicios/sistemas/sql/sistema.dql.sql
@@ -1,5 +1,8 @@
--- Traeme las veces que se ejecuto un programa con frecuencia media en un sistema de nivel bajo
-
+-- Traeme la cantidad de veces que se ejecuto un programa con frecuencia media en un sistema de nivel bajo
+select count(*) as cantidad, frec_ejec, nivel from programas
+join sistema_programa on sistema_programa.frec_ejec_programa = programas.frec_ejec
+join sistemas on sistema_programa.nivel_sistema = sistemas.nivel
+where (nivel = "bajo" and frec_ejec="media");
 
 -- Traeme la suma de veces que se ejecutaron programas en el sistema
 
@@ -7,7 +10,7 @@
 -- Traeme los programas con archivos input_output
 
 
-/* 
+/*
 	Traeme las frecuencia de los programas con archivos 
 	input_output y el nivel de su sistema 
-*/															
+*/														
